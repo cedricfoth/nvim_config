dd7637b8796b01084cb6da81b3ebb2f8e0d6ef77
Cedric Foth <cedric.foth@tuhh.de>
2024-06-18 13:11:33 +0200

    Reenable relativenumber feature.

--- lua/general/general.lua
+++ lua/general/general.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
---vim.cmd("set relativenumber")
+vim.cmd("set relativenumber")
 vim.cmd("set clipboard=unnamedplus")
 vim.g.mapleader = " "
 vim.g.mpalocalleader = " "
