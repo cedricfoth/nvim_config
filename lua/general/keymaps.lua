a685a94596a73a27e3cc7546130c2f3a2c0681ba
Cedric Foth <cedric.foth@tuhh.de>
2024-07-07 14:44:32 +0200

    Updated Keymaps description.

--- lua/general/keymaps.lua
+++ lua/general/keymaps.lua
@@ -8,17 +8,24 @@
 --vim.keymap.set('n','<c-k>',':wincmd k<CR>',{}) -- Move Up
 
 -- -- Split
-vim.keymap.set('n','<Leader>sh','<C-w>s',{}) -- Split Horizontal
-vim.keymap.set('n','<Leader>sv','<C-w>v',{}) -- Split Vertical
-vim.keymap.set('n','<Leader>sx','<C-w>q',{}) -- Quit Split
+vim.keymap.set("n", "<Leader>sh", "<C-w>s", { desc = "Horizontal Split" }) -- Split Horizontal
+vim.keymap.set("n", "<Leader>sv", "<C-w>v", { desc = "Vertical Split" })   -- Split Vertical
+vim.keymap.set("n", "<Leader>sx", "<C-w>q", { desc = "Quit Split" })       -- Quit Split
 
 -- -- Vimtext
-vim.keymap.set("n", "<leader>qc", ":VimtexCompile<CR><CR>")
-vim.keymap.set("n", "<leader>qs", ":!zathura --fork %:t:r.pdf<CR><CR>")
-vim.keymap.set("n","<leader>tt", ":VimtexTocToggle<CR>")
+vim.keymap.set("n", "<leader>qc", ":VimtexCompile<CR><CR>", { desc = "Compile Latex Document", silent = true })
+vim.keymap.set(
+  "n",
+  "<leader>qs",
+  ":!zathura --fork %:t:r.pdf<CR><CR>",
+  { desc = "Open zathura for current document", silent = true }
+)
+vim.keymap.set("n", "<leader>tt", ":VimtexTocToggle<CR>", { desc = "Toggle TOC", silent = true })
 
+--Normal Mode
+vim.keymap.set("n", "<leader>kj", ":nohl<cr>", { desc = "Clear Hightlights", silent = true })
 
 --Insert Mode
-vim.keymap.set('i','kj','<Esc>',{})
+vim.keymap.set("i", "kj", "<Esc>", {})
 --Visual Mode
-vim.keymap.set('v','kj','<Esc>',{})
+vim.keymap.set("v", "kj", "<Esc>", {})
