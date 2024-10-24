819dfab2ad054ca64754c0e315c48e29b24cb547
Cedric Foth <cedric.foth@tuhh.de>
2024-10-24 13:57:02 +0200

    Added markdown weekly snippet

--- lua/plugins/snippets/markdown.lua
+++ lua/plugins/snippets/markdown.lua
@@ -3,5 +3,10 @@ return{
     t({"---","date: "..vim.fn.strftime("%Y-%m-%d"), "tags: "}),
     i(1),
     t({"","hubs:","---"})
+  }),
+  s("weekly", {
+    t({"### "..vim.fn.strftime("%Y-%m-%d"), "Fragen: "}),
+    i(1),
+    t({"#### Informationen/Notes:","", "#### Wichtige Punkte:","","#### Zusammenfassung:", ""})
   })
 }
