7e591fb7bd1db70318dcee56e277febbef82eeb3
Cedric Foth <cedric.foth@tuhh.de>
2024-10-24 13:42:26 +0200

    Commit with breaking changes

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,42 @@
+return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	lazy = false,
+	version = false, -- set this if you want to always pull the latest change
+	opts = {
+		provider = "gemini", -- Recommend using Claude
+		-- auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+		behaviour = {
+			auto_suggestions = false, -- Experimental stage
+			auto_set_highlight_group = false,
+			auto_set_keymaps = true,
+			auto_apply_diff_after_generation = false,
+			support_paste_from_clipboard = false,
+		},
+		gemini = {
+			-- @see https://ai.google.dev/gemini-api/docs/models/gemini
+			model = "gemini-1.5-pro-exp-0827",
+			-- model = "gemini-1.5-flash",
+			temperature = 0,
+			max_tokens = 4096,
+		},
+	},
+	behaviour = {
+		auto_suggestions = false, -- Experimental stage
+	},
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	build = "make",
+	dependencies = {
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			"MeanderingProgrammer/render-markdown.nvim",
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
+}
