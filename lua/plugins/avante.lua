0c0f7af8e6e7f9b23c28d22bd1bbf81bf7cb913a
Cedric Foth <cedric.foth@tuhh.de>
2025-02-21 12:23:26 +0100

    UPDATE(avante): updated

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,38 +1,41 @@
 return {
-	"yetone/avante.nvim",
-	event = "VeryLazy",
-	lazy = false,
-	version = false, -- set this if you want to always pull the latest change
-	opts = {
-		provider = "gemini", -- Recommend using Claude
-		-- auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-		behaviour = {
-			auto_suggestions = false, -- Experimental stage
-			auto_set_highlight_group = true,
-			auto_set_keymaps = true,
-			auto_apply_diff_after_generation = false,
-			support_paste_from_clipboard = true,
-		},
-		gemini = {
-			-- @see https://ai.google.dev/gemini-api/docs/models/gemini
-			model = "gemini-1.5-pro",
-			-- model = "gemini-1.5-flash",
-			temperature = 0,
-			max_tokens = 4096,
-		},
-	},
-	behaviour = {
-		auto_suggestions = false, -- Experimental stage
-	},
-	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-build = "make",
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false,     -- set this if you want to always pull the latest change
+  opts = {
+    provider = "gemini", -- Recommend using Claude
+    -- auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+    behaviour = {
+      auto_suggestions = false, -- Experimental stage
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = true,
+    },
+    -- openai = {
+    --   model = "gpt-4o-2024-08-06",
+    -- },
+    gemini = {
+      -- @see https://ai.google.dev/gemini-api/docs/models/gemini
+      model = "gemini-1.5-pro",
+    --   -- model = "gemini-1.5-flash",
+    --   temperature = 0,
+    --   max_tokens = 4096,
+    },
+  },
+  behaviour = {
+    auto_suggestions = false, -- Experimental stage
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "hrsh7th/nvim-cmp",          -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     {
       -- support for image pasting
@@ -53,7 +56,7 @@ build = "make",
     },
     {
       -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
+      "MeanderingProgrammer/render-markdown.nvim",
       opts = {
         file_types = { "markdown", "Avante" },
       },
