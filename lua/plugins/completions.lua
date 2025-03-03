06bd770dc774a13ecd1aa9a6a116bae921de2496
Cedric Foth <cedric.foth@tuhh.de>
2025-03-03 18:18:12 +0100

    UPDATE(general): General adding from cmp, flash and haproon

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -53,6 +53,27 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<C-n>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif ls.expand_or_jumpable() then
+              ls.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<C-p>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif ls.jumpable(-1) then
+              ls.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
