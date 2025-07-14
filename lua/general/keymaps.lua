bc1531a1922a9e525b81d4f93dce312b78633450
Cedric Foth <cedric.foth@tuhh.de>
2025-07-14 11:46:16 +0200

    Added GH and GL for MR and PR.

--- lua/general/keymaps.lua
+++ lua/general/keymaps.lua
@@ -27,7 +27,6 @@ keymap("n", "|-", ":res -5<cr>",      { silent = true, desc = "Decrease window h
 keymap("n", "_+", ":vert res +5<cr>", { silent = true, desc = "Increase window width" })
 keymap("n", "_-", ":vert res -5<cr>", { silent = true, desc = "Decrease window width" })
 
-
 -- -- Tabs
 keymap("n", "<Leader>tn", ":tabnew<CR>", { desc = "New Tab",silent = true }) -- New Tab
 keymap("n", "<Leader>qt", ":tabc<CR>", { desc = "Close Tab", silent = true }) -- Close Tab
@@ -61,7 +60,7 @@ keymap("n", "<leader>gc", ":DiffviewClose<cr>", { desc = "Close Diffview", silen
 keymap("i","<C-l>","<Plug>(copilot-accept-word)",{silent = true})
 
 vim.g.copilot_no_tab_map = true
-keymap('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+keymap('i', '<S-Tab>', 'copilot#Accept("\\<CR>")', {
   expr = true,
   replace_keycodes = false
 })
@@ -93,7 +92,7 @@ keymap("n", "<leader>kj", ":nohl<cr>", { desc = "Clear Hightlights", silent = tr
 keymap("n", "<C-u>", "<C-u>zz", { desc = "Clear Hightlights", silent = true })
 keymap("n", "<C-d>", "<C-d>zz", { desc = "Clear Hightlights", silent = true })
 keymap("n", "<C-s>", ":s/<C-r><C-w>/", { desc = "Replace word under Cursor"})
-keymap("n", "<leader>cf", ":ClearQuickfixList", { desc = "Clear QuickFix List"})
+keymap('n', '<leader>cq', ':call setqflist([])<CR>:cclose<CR>', { noremap = true, silent = true })
 keymap("n", "<leader>bd", ":Bdelete<cr>", { silent = true, desc = "Close current buffer" })
 keymap("n", "<bs>", "<C-^>", { silent = true, desc = "Switch to previous buffer" })
 keymap("n", "<leader>qq", ":q<cr>", { silent = true, desc = "Close current buffer" })
@@ -116,3 +115,41 @@ keymap("n", "N", "Nzzzv", { silent = true, desc = "Move to previous search match
 keymap("x", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move visual block down" })
 keymap("x", "K", ":m '<-2<CR>gv=gv", { silent = true, desc = "Move visual block up" })
 
+-- =============================================================================
+-- Octo.nvim Shortcuts -- Leader + r (für Review/Repo)
+-- =============================================================================
+--
+local map = function(lhs, rhs, desc)
+  vim.keymap.set('n', lhs, rhs, { silent = true, desc = 'Octo: ' .. desc })
+end
+map('ghr', '<cmd>Octo review start<cr>', 'Start new review')
+map('ghR', '<cmd>Octo review submit<cr>', 'Submit review')
+map('ghd', '<cmd>Octo review close<cr>', 'Discard review')
+map('ghca', '<cmd>Octo review comment<cr>', 'Add review comment')
+map('ghcs', '<cmd>Octo review suggestion<cr>', 'Add review suggestion')
+
+-- Allgemeine Kommentar-Aktionen
+map('ca', '<cmd>Octo comment add<cr>', 'Add a standalone comment')
+map('cd', '<cmd>Octo comment delete<cr>', 'Delete comment/suggestion')
+
+-- Hilfsfunktion, um eine Issue/PR-Nummer aus der visuellen Auswahl oder dem Register zu öffnen
+local function open_octo_item(item_type)
+  local number = vim.fn.getreg('"') -- Nummer aus dem Register holen
+  if vim.fn.mode() == 'v' then -- Wenn im visuellen Modus, den ausgewählten Text nehmen
+    vim.cmd('noautocmd silent! normal! "vy"')
+    number = vim.fn.getreg('v')
+  end
+  vim.cmd('Octo ' .. item_type .. ' open ' .. vim.fn.trim(number))
+end
+
+-- Pull Requests
+map('ghpl', '<cmd>Telescope octo pull_requests<cr>', 'List Pull Requests')
+map('ghpa', '<cmd>Telescope octo pull_requests assignee=@me<cr>', 'List my assigned PRs')
+map('ghpc', '<cmd>Octo pr create<cr>', 'Create Pull Request')
+map('ghpb', '<cmd>Octo pr checkout<cr>', 'Checkout PR branch')
+
+-- Issues
+map('ghil', '<cmd>Telescope octo issues<cr>', 'List Issues')
+map('ghia', '<cmd>Telescope octo issues assignee=@me<cr>', 'List my assigned Issues')
+map('ghic', '<cmd>Octo issue create<cr>', 'Create Issue')
+map('ghio', function() open_octo_item('issue') end, 'Open Issue # (from selection/clipboard)')
