e0464dffff9b800071a755744dd85fe6587c21d6
Cedric Foth <cedric.foth@tuhh.de>
2025-07-14 11:45:54 +0200

    Updated LSP Config.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,16 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-      },
-    })
-
-  end,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.yamlfmt,
+				null_ls.builtins.formatting.clang_format.with({
+                    extra_args = { "--style=Google" },
+                }),
+			},
+		})
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -62,11 +62,11 @@ return {
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Fuzzy find in Buffers" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fg", ":lua require('telescope.builtin').live_grep({ additional_args = { '--fixed-strings' }})<CR>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
     keymap.set("n", "<leader>fz", "<cmd>Telescope bibtex <cr>", { desc = "Find Literature" })
-    keymap.set("n", "<leader>fp", "<cmd>Telescope bibtex format_string=\\citep{%s}<cr>", { desc = "Find git commits" })
+    keymap.set("n", "<leader>fp", "<cmd>Telescope bibtex format_string=\\citep{%s}<cr>", { desc = "Find Lieterature (Format \\cite{})" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope git_commits<cr>", { desc = "Find Lieterature (Format \\cite{})" })
   end,
 }
