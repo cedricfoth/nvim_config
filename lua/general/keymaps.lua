57d1bb511fa51f399fd23363cf33f04cbecfec23
Cedric Foth <cedric.foth@tuhh.de>
2026-01-04 17:33:51 +0100

    Updated general config

--- lua/general/keymaps.lua
+++ lua/general/keymaps.lua
@@ -4,41 +4,45 @@
 --vim.keymap.set('n','<c-l>',':wincmd l<CR>',{}) -- Move Right
 --vim.keymap.set('n','<c-j>',':wincmd j<CR>',{}) --Move Down
 --vim.keymap.set('n','<c-k>',':wincmd k<CR>',{}) -- Move Up
+--
+local select_block = require("utils.select_codeblock")
 
 local keymap = vim.keymap.set
 
-keymap({'n', 'v', 'o'}, 'ä', ']', { remap = true })
-keymap({'n', 'v', 'o'}, 'ö', '[', { remap = true })
+keymap({ "n", "v", "o" }, "ä", "]", { remap = true })
+keymap({ "n", "v", "o" }, "ö", "[", { remap = true })
 keymap("n", "ü", "'", { remap = true })
 
 -- -- Split
 keymap("n", "||", ":vsplit<cr>", { silent = true, desc = "Split window vertically" })
 keymap("n", "--", ":split<cr>", { silent = true, desc = "Split window horizontally" })
-keymap("n", "<Leader>sx", "<C-w>q", { desc = "Quit Split" }) -- Quit Split
-keymap("n", "=", "<C-w>=", { desc = "Equal Size" }) -- Equal Size
-keymap("n", "<Leader>sm", "<C-w>T", { desc = "Open in Tab" }) -- Open in Tab
+keymap("n", "<Leader>wq", "<C-w>q", { desc = "Quit Split" }) -- Quit Split
+keymap("n", "<Leader>we", "<C-w>=", { desc = "Equal Size" }) -- Equal Size
+keymap("n", "<Leader>wt", "<C-w>T", { desc = "Open in Tab" }) -- Open in Tab
 -- -- Window Resizing
-keymap("n", "tl", ":vert res +15<cr>", { silent = true, desc = "Increase window height" })
-keymap("n", "th", ":vert res -15<cr>", { silent = true, desc = "Decrease window height" })
-keymap("n", "tk", ":res +15<cr>", { silent = true, desc = "Increase window width" })
-keymap("n", "tj", ":res -15<cr>", { silent = true, desc = "Decrease window width" })
+keymap("n", "<Leader>wl", ":vert res +15<cr>", { silent = true, desc = "Increase window height" })
+keymap("n", "<Leader>wh", ":vert res -15<cr>", { silent = true, desc = "Decrease window height" })
+keymap("n", "<Leader>wk", ":res +15<cr>", { silent = true, desc = "Increase window width" })
+keymap("n", "<Leader>wj", ":res -15<cr>", { silent = true, desc = "Decrease window width" })
 
 -- -- Tabs
-keymap("n", "<Leader>nt", ":tabnew<CR>", { desc = "New Tab", silent = true }) -- New Tab
-keymap("n", "<Leader>qt", ":tabc<CR>", { desc = "Close Tab", silent = true }) -- Close Tab
+keymap("n", "<Leader>Tn", ":tabnew<CR>", { desc = "New Tab", silent = true }) -- New Tab
+keymap("n", "<Leader>Tq", ":tabc<CR>", { desc = "Close Tab", silent = true }) -- Close Tab
+
 -- keymap("n", "ää", ":tabnext<CR>", { desc = "Next Tab", silent = true }) -- Next Tab
 -- keymap("n", "öö", ":tabprevious<CR>", { desc = "Previous Tab", silent = true }) -- Previous Tab
 
 -- -- Vimtext
-keymap("n", "<leader>tc", ":VimtexCompile<CR><CR>", { desc = "Compile Latex Document", silent = true })
+keymap("n", "<leader>vc", ":VimtexCompile<CR>", { desc = "Compile Latex Document", silent = true })
 keymap(
 	"n",
-	"<leader>tz",
-	":!zathura --fork %:t:r.pdf<CR><CR>",
+	"<leader>vz",
+	":!zathura  --fork --synctex-forward %l:1:%f %p<CR><CR>",
 	{ desc = "Open zathura for current document", silent = true }
 )
-keymap("n", "<leader>tt", ":VimtexTocToggle<CR>", { desc = "Toggle TOC", silent = true })
-keymap("n", "<leader>ts", ":VimtexView<CR>", { desc = "Show in PDF", silent = true })
+keymap("n", "<leader>vt", ":VimtexTocToggle<CR>", { desc = "Toggle TOC", silent = true })
+keymap("n", "<leader>vs", ":VimtexView<CR>", { desc = "Show in PDF", silent = true })
+keymap("n", "<leader>ve", ":VimtexErrors<CR>", { desc = "Vimtex Errors", silent = true })
 
 -- Obisidian commands
 keymap("n", "<leader>oo", ":ObsidianOpen<cr>", { desc = "Open current File in Obsidian", silent = true })
@@ -65,25 +69,26 @@ keymap("x", "<leader>rf", ":Refactor extract_to_file ", { desc = "Extract Functi
 
 keymap("x", "<leader>rv", ":Refactor extract_var ", { desc = "Extract Variable" })
 
-keymap({ "n", "x" }, "<leader>ri", ":Refactor inline_var", { desc = "Inline Variable" })
+keymap({ "n", "x" }, "<leader>ri", ":Refactor inline_var ", { desc = "Inline Variable" })
 
-keymap("n", "<leader>rI", ":Refactor inline_func", { desc = "Inline Function" })
+keymap("n", "<leader>rI", ":Refactor inline_func ", { desc = "Inline Function" })
 
-keymap("n", "<leader>rb", ":Refactor extract_block", { desc = "Extract Block" })
-keymap("n", "<leader>rbf", ":Refactor extract_block_to_file", { desc = "Extract Block to File" })
+keymap("n", "<leader>rb", ":Refactor extract_block ", { desc = "Extract Block" })
+keymap("n", "<leader>rbf", ":Refactor extract_block_to_file ", { desc = "Extract Block to File" })
 
 -- Undotree
 keymap("n", "<leader>u", ":UndotreeToggle<cr>", { desc = "Toggle Undotree", silent = true })
 
 --Normal Mode
-keymap("n", "<leader>kj", ":nohl<cr>", { desc = "Clear Hightlights", silent = true })
+keymap("n", "<leader>lh", ":nohl<cr>", { desc = "Clear Hightlights", silent = true })
 keymap("n", "<C-u>", "<C-u>zz", { desc = "Clear Hightlights", silent = true })
 keymap("n", "<C-d>", "<C-d>zz", { desc = "Clear Hightlights", silent = true })
 keymap("n", "<C-s>", ":s/<C-r><C-w>/", { desc = "Replace word under Cursor" })
-keymap("n", "<leader>cq", ":call setqflist([])<CR>:cclose<CR>", { noremap = true, silent = true })
+keymap("n", "<leader>qq", ":call setqflist([])<CR>:cclose<CR>", { noremap = true, silent = true })
 keymap("n", "<bs>", "<C-^>", { silent = true, desc = "Switch to previous buffer" })
-keymap("n", "<leader>qq", ":bd<cr>", { silent = true, desc = "Close current buffer" })
-keymap("n", "<leader>qa", ":%bd<cr>", { silent = true, desc = "Close all buffers" })
+keymap("n", "<leader>bq", ":bd<cr>", { silent = true, desc = "Close current buffer" })
+keymap("n", "<leader>ba", ":%bd<cr>", { silent = true, desc = "Close all buffers" })
+
 keymap("n", "J", "mzJ`z", { silent = true, desc = "Append next line to current line with space" })
 keymap("n", "gp", "`[v`]", { silent = true, desc = "Re-select last pasted text" })
 
@@ -98,7 +103,7 @@ keymap(
 --Insert Mode
 keymap("i", "kj", "<Esc>", {})
 --Visual Mode
-keymap("v", "kj", "<Esc>", {})
+keymap("v", "<C-k>", "<Esc>", {})
 -- Search Mode
 keymap("n", "n", "nzzzv", { silent = true, desc = "Move to next search match and center" })
 keymap("n", "N", "Nzzzv", { silent = true, desc = "Move to previous search match and center" })
@@ -107,73 +112,79 @@ keymap("n", "N", "Nzzzv", { silent = true, desc = "Move to previous search match
 keymap("x", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move visual block down" })
 keymap("x", "K", ":m '<-2<CR>gv=gv", { silent = true, desc = "Move visual block up" })
 
--- Hilfsfunktion, um eine Issue/PR-Nummer aus der visuellen Auswahl oder dem Register zu öffnen
-local function open_octo_item(item_type)
-	local number = vim.fn.getreg('"') -- Nummer aus dem Register holen
-	if vim.fn.mode() == "v" then -- Wenn im visuellen Modus, den ausgewählten Text nehmen
-		vim.cmd('noautocmd silent! normal! "vy"')
-		number = vim.fn.getreg("v")
-	end
-	vim.cmd("Octo " .. item_type .. " open " .. vim.fn.trim(number))
-end
+-- -- Hilfsfunktion, um eine Issue/PR-Nummer aus der visuellen Auswahl oder dem Register zu öffnen
+-- local function open_octo_item(item_type)
+-- 	local number = vim.fn.getreg('"') -- Nummer aus dem Register holen
+-- 	if vim.fn.mode() == "v" then -- Wenn im visuellen Modus, den ausgewählten Text nehmen
+-- 		vim.cmd('noautocmd silent! normal! "vy"')
+-- 		number = vim.fn.getreg("v")
+-- 	end
+-- 	vim.cmd("Octo " .. item_type .. " open " .. vim.fn.trim(number))
+-- end
 
--- Pull Requests
-keymap("n", ",ghpl", "<cmd>Telescope octo pull_requests<cr>", { silent = true, desc = "Octo: List Pull Requests" })
-keymap(
-	"n",
-	",ghpa",
-	"<cmd>Telescope octo pull_requests assignee=@me<cr>",
-	{ silent = true, desc = "Octo: List my assigned PRs" }
-)
-keymap("n", ",ghpc", "<cmd>Octo pr create<cr>", { silent = true, desc = "Octo: Create Pull Request" })
-keymap("n", ",ghpb", "<cmd>Octo pr checkout<cr>", { silent = true, desc = "Octo: Checkout PR branch" })
-
--- Issues
-keymap("n", ",ghil", "<cmd>Telescope octo issues<cr>", { silent = true, desc = "Octo: List Issues" })
-keymap(
-	"n",
-	",ghia",
-	"<cmd>Telescope octo issues assignee=@me<cr>",
-	{ silent = true, desc = "Octo: List my assigned Issues" }
-)
-keymap("n", ",ghic", "<cmd>Octo issue create<cr>", { silent = true, desc = "Octo: Create Issue" })
-keymap("n", ",ghio", function()
-	open_octo_item("issue")
-end, { silent = true, desc = "Octo: Open Issue # (from selection/clipboard)" })
+-- -- Pull Requests
+-- keymap("n", ",ghpl", "<cmd>Telescope octo pull_requests<cr>", { silent = true, desc = "Octo: List Pull Requests" })
+-- keymap(
+-- 	"n",
+-- 	",ghpa",
+-- 	"<cmd>Telescope octo pull_requests assignee=@me<cr>",
+-- 	{ silent = true, desc = "Octo: List my assigned PRs" }
+-- )
+-- keymap("n", ",ghpc", "<cmd>Octo pr create<cr>", { silent = true, desc = "Octo: Create Pull Request" })
+-- keymap("n", ",ghpb", "<cmd>Octo pr checkout<cr>", { silent = true, desc = "Octo: Checkout PR branch" })
+--
+-- -- Issues
+-- keymap("n", ",ghil", "<cmd>Telescope octo issues<cr>", { silent = true, desc = "Octo: List Issues" })
+-- keymap(
+-- 	"n",
+-- 	",ghia",
+-- 	"<cmd>Telescope octo issues assignee=@me<cr>",
+-- 	{ silent = true, desc = "Octo: List my assigned Issues" }
+-- )
+-- keymap("n", ",ghic", "<cmd>Octo issue create<cr>", { silent = true, desc = "Octo: Create Issue" })
+-- keymap("n", ",ghio", function()
+-- 	open_octo_item("issue")
+-- end, { silent = true, desc = "Octo: Open Issue # (from selection/clipboard)" })
 
 -- LSP Keymaps
 
-keymap("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true, silent = true, desc = "Rename" })
+keymap("n", "<leader>lr", vim.lsp.buf.rename, { noremap = true, silent = true, desc = "Rename" })
 keymap("n", "<leader>lx", ":LspStop<cr>", { noremap = true, silent = true, desc = "Stop LSP Client" })
 keymap("n", "<leader>ls", ":LspStart<cr>", { noremap = true, silent = true, desc = "Start LSP Client" })
-keymap("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true, desc = "Hover" })
+keymap("n", "K", function()
+	vim.lsp.buf.hover({ border = "single", max_height = 25, max_width = 120 })
+end, { desc = "Hover Documentation" })
 keymap("n", "gD", vim.lsp.buf.declaration, { noremap = true, silent = true, desc = "Go to Declaration" })
 keymap("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true, desc = "Go to Definition" })
-keymap("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true, desc = "Go to Reference" })
+-- keymap("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true, desc = "Go to Reference" }) -- Deprecated due to new Lspsaga keymap
 keymap("n", "]d", ":Lspsaga diagnostic_jump_next <cr>", { desc = "Next Diagnostic", silent = true })
 keymap("n", "[d", ":Lspsaga diagnostic_jump_prev <cr>", { desc = "Previous Diagnostic", silent = true })
-keymap({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-keymap("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show Diagnostics" })
-keymap("n", "gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
+keymap({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "Code Action" })
+keymap("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Show Diagnostics" })
+-- keymap("n", "gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" }) -- Deprecated due to new Lspsaga keymap
 
 -- Molten
--- keymap("n", "<leader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" })
-keymap("n", "<leader>me", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "run operator selection" })
-keymap("n", "<leader>ml", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
-keymap("n", "<leader>mr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
+keymap("n", "<leader>je", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "Jupyter: run operator selection" })
+keymap("n", "<leader>jl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "Jupyter: evaluate line" })
+keymap("n", "<leader>jr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "Jupyter: re-evaluate cell" })
 keymap(
 	"v",
-	"<leader>mv",
+	"<leader>jv",
 	":<C-u>MoltenEvaluateVisual<CR>gv",
-	{ silent = true, desc = "evaluate visual selection" }
+	{ silent = true, desc = "Jupyter: evaluate visual selection" }
 )
-keymap("n", "<leader>mi", function()
-  local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
-  if venv ~= nil then
-    -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
-    venv = string.match(venv, "/.+/(.+)")
-    vim.cmd(("MoltenInit %s"):format(venv))
-  else
-    vim.cmd("MoltenInit python3")
-  end
-end, { desc = "Initialize Molten for python3", silent = true })
+keymap("n", "<leader>ji", function()
+	local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
+	if venv ~= nil then
+		-- in the form of /home/benlubas/.virtualenvs/VENV_NAME
+		venv = string.match(venv, "/.+/(.+)")
+		vim.cmd(("MoltenInit %s"):format(venv))
+	else
+		vim.cmd("MoltenInit python3")
+	end
+end, { desc = "Jupyter: Initialize Molten for python3", silent = true })
+
+--- Custom Script Commands
+keymap({ "n", "v" }, "<leader>jbv", select_block.select_inner_code_block, {
+	desc = "Select inner Markdown code block",
+})
