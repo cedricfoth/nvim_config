935da627ac36b05109f0f1f4b619f569ef27ff3d
Cedric Foth <cedric.foth@tuhh.de>
2026-01-04 17:36:00 +0100

    Formatted files

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,49 +1,58 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local colors = {
-      blue = "#80a0ff",
-      cyan = "#79dac8",
-      black = "#080808",
-      white = "#c6c6c6",
-      red = "#ff5189",
-      violet = "#d183e8",
-      grey = "#303030",
-    }
-    local bubbles_theme = {
-      normal = {
-        a = { fg = colors.black, bg = colors.violet },
-        b = { fg = colors.white, bg = colors.grey },
-        c = { fg = colors.white },
-      },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local colors = {
+			blue = "#80a0ff",
+			cyan = "#79dac8",
+			black = "#080808",
+			white = "#c6c6c6",
+			red = "#ff5189",
+			violet = "#d183e8",
+			grey = "#303030",
+		}
+		local bubbles_theme = {
+			normal = {
+				a = { fg = colors.black, bg = colors.violet },
+				b = { fg = colors.white, bg = colors.grey },
+				c = { fg = colors.white },
+			},
 
-      insert = { a = { fg = colors.black, bg = colors.blue } },
-      visual = { a = { fg = colors.black, bg = colors.cyan } },
-      replace = { a = { fg = colors.black, bg = colors.red } },
+			insert = { a = { fg = colors.black, bg = colors.blue } },
+			visual = { a = { fg = colors.black, bg = colors.cyan } },
+			replace = { a = { fg = colors.black, bg = colors.red } },
 
-      inactive = {
-        a = { fg = colors.white, bg = colors.black },
-        b = { fg = colors.white, bg = colors.black },
-        c = { fg = colors.white },
-      },
-    }
+			inactive = {
+				a = { fg = colors.white, bg = colors.black },
+				b = { fg = colors.white, bg = colors.black },
+				c = { fg = colors.white },
+			},
+		}
 
-    require("lualine").setup({
-      sections = {
-        lualine_x = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-            color = { fg = "#ff9e64" },
-          },
-        },
-      },
-      options = {
-        theme = "catppuccin",
-        component_separators = "",
-        section_separators = { left = "", right = "" },
-      },
-    })
-  end,
+		require("lualine").setup({
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+				lualine_b = {
+					{
+						function()
+							return vim.g.remote_neovim_host and ("Remote: %s"):format(vim.uv.os_gethostname()) or ""
+						end,
+						padding = { right = 1, left = 1 },
+						separator = { left = "", right = "" },
+					},
+				},
+			},
+			options = {
+				theme = "catppuccin",
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+			},
+		})
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,34 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = {
-                "bibtex",
-                "cmake",
-                "cpp",
-                "html",
-                "yaml",
-                "python",
-                "lua",
-                "json",
-                "javascript",
-                "tsx",
-                "css",
-                "bash",
-                "latex",
-                "vim",
-                "matlab",
-                "markdown",
-                "markdown_inline",
-                "gitignore",
-                "json",
-                "terraform",
-                "toml",
-            },
-            highlight = { enable = true },
-            indent = { enable = false },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"bibtex",
+				"cmake",
+				"cpp",
+				"html",
+				"yaml",
+				"python",
+				"lua",
+				"json",
+				"javascript",
+				"tsx",
+				"css",
+				"bash",
+				"latex",
+				"vim",
+				"matlab",
+				"markdown",
+				"markdown_inline",
+				"gitignore",
+				"json",
+				"terraform",
+				"toml",
+			},
+			highlight = { enable = true, disable = { "latex" } },
+			indent = { enable = false },
+		})
+	end,
 }
