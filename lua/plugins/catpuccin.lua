6c1705645334e99d0bc595fb80c0c8f660f6493e
Cedric Foth <cedric.foth@tuhh.de>
2026-01-04 17:34:37 +0100

    Updated Color scheme behavior

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,8 +1,26 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme("catppuccin")
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {},
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			transparent_background = false, -- Hintergrund transparent
+			float = {
+				transparent = false, -- transparente Floating-Windows
+				solid = false,      -- kein solides Styling
+			},
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = false,
+				telescope = false,
+				treesitter = true,
+				which_key = false,
+			},
+		})
+		-- Colorscheme erst nach setup aktivieren
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
