648b510ea4aed23a60351bea9f075ec0f13350e6
Cedric Foth <cedric.foth@tuhh.de>
2026-01-04 17:36:28 +0100

    Updated inline diagnostics

--- lua/plugins/inline-diagnostics.lua
+++ lua/plugins/inline-diagnostics.lua
@@ -3,7 +3,12 @@ return{
     event = "VeryLazy",
     priority = 1000,
     config = function()
-        require('tiny-inline-diagnostic').setup()
+        require('tiny-inline-diagnostic').setup({
+            preset = 'modern',
+            transparent_bg = false,
+            transparent_cursorline = false,
+        })
         vim.diagnostic.config({ virtual_text = false }) -- Disable default virtual text
+
     end
 }
