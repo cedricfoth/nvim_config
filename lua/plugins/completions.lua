06c3dc6c659ff4621ff7aa09f3fbefae97efc472
Cedric Foth <cedric.foth@tuhh.de>
2026-01-04 17:34:53 +0100

    Updated cmp boxes

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,81 +1,86 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-      local ls = require("luasnip")
-      ls.setup({
-        snip_env = {
-          s = function(...)
-            local snip = ls.s(...)
-            -- we can't just access the global `ls_file_snippets`, since it will be
-            -- resolved in the environment of the scope in which it was defined.
-            table.insert(getfenv(2).ls_file_snippets, snip)
-          end,
-          parse = function(...)
-            local snip = ls.parser.parse_snippet(...)
-            table.insert(getfenv(2).ls_file_snippets, snip)
-          end,
-          -- remaining definitions.
-        },
-      })
+			local ls = require("luasnip")
+			ls.setup({
+				snip_env = {
+					s = function(...)
+						local snip = ls.s(...)
+						table.insert(getfenv(2).ls_file_snippets, snip)
+					end,
+					parse = function(...)
+						local snip = ls.parser.parse_snippet(...)
+						table.insert(getfenv(2).ls_file_snippets, snip)
+					end,
+				},
+			})
 
-      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/plugins/snippets" })
+			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/plugins/snippets" })
 
-      cmp.setup({
-        snippet = {
-          -- REQUIRED - you must specify a snippet engine
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-n>"] = cmp.mapping(function(fallback)
-            if ls.expand_or_jumpable() then
-              ls.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+			cmp.setup({
+				snippet = {
+					-- REQUIRED - you must specify a snippet engine
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
 
-          ["<C-p>"] = cmp.mapping(function(fallback)
-            if ls.jumpable(-1) then
-              ls.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
+				window = {
+					completion = cmp.config.window.bordered({
+						border = "rounded",
+						winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+					}),
+					documentation = cmp.config.window.bordered({
+						border = "rounded",
+						winhighlight = "Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+					}),
+				},
+
+				mapping = cmp.mapping.preset.insert({
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<C-n>"] = cmp.mapping(function(fallback)
+						if ls.expand_or_jumpable() then
+							ls.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<C-p>"] = cmp.mapping(function(fallback)
+						if ls.jumpable(-1) then
+							ls.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "path" },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
