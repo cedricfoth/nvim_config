b12c6ab2d789aaa01610e514707d21ba605e33be
Cedric Foth <cedric.foth@tuhh.de>
2026-01-06 16:27:24 +0100

    Edit color theme

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -8,19 +8,25 @@ return {
 			flavour = "mocha", -- latte, frappe, macchiato, mocha
 			transparent_background = false, -- Hintergrund transparent
 			float = {
-				transparent = false, -- transparente Floating-Windows
-				solid = false,      -- kein solides Styling
+				transparent = true, -- transparente Floating-Windows
+				solid = false, -- kein solides Styling
 			},
 			integrations = {
 				cmp = true,
 				gitsigns = true,
-				nvimtree = false,
-				telescope = false,
+				neotree = true,
+				telescope = true,
 				treesitter = true,
-				which_key = false,
+				which_key = true,
 			},
 		})
 		-- Colorscheme erst nach setup aktivieren
 		vim.cmd.colorscheme("catppuccin")
+		function LineNumberColors()
+			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#b4befe", bold = false })
+			vim.api.nvim_set_hl(0, "LineNr", { fg = "#fab387", bold = true })
+			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#b4befe", bold = false })
+		end
+        LineNumberColors()
 	end,
 }
