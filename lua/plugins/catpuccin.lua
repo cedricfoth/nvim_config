d20d06b04030922748838a5e0aaabacdf73e03be
Cedric Foth <cedric.foth@tuhh.de>
2026-02-16 11:16:04 +0100

    Updated(theme): Catpuccin theme to match overall feeling

--- lua/plugins/catpuccin.lua
+++ lua/plugins/catpuccin.lua
@@ -1,61 +1,15 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	opts = {},
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-			transparent_background = false, -- Hintergrund transparent
-			float = {
-				transparent = true, -- transparente Floating-Windows
-				solid = false, -- kein solides Styling
-			},
-			integrations = {
-				gitsigns = true,
-				neotree = true,
-				telescope = true,
-				treesitter = true,
-				which_key = true,
-                notify = true,
-                snacks = true,
-			},
-		})
-		-- Colorscheme erst nach setup aktivieren
-		vim.cmd.colorscheme("catppuccin")
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
 
-		function LineNumberColors()
-			local palette = require("catppuccin.palettes").get_palette()
-			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#b4befe", bold = false })
-			vim.api.nvim_set_hl(0, "LineNr", { fg = "#fab387", bold = true })
-			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#b4befe", bold = false })
-			vim.api.nvim_set_hl(0, "Pmenu", { bg = none })
-
-			vim.api.nvim_set_hl(0, "BlinkCmpMenu", {
-				bg = palette.base,
-				fg = palette.text,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
 			})
-
-			vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", {
-				bg = palette.surface0,
-				fg = palette.text,
-			})
-
-			vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", {
-				fg = palette.blue,
-				bg = palette.base,
-			})
-
-			vim.api.nvim_set_hl(0, "BlinkCmpDoc", {
-				bg = palette.base,
-				fg = palette.text,
-			})
-
-			vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", {
-				fg = palette.blue,
-				bg = palette.base,
-			})
-		end
-		LineNumberColors()
-	end,
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
